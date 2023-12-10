e8152a0c5cb36c96145af680cd20906c0d5d63b4
GodNpc <123784547+Wh1t3Bl4ckPT@users.noreply.github.com>
2023-12-10 19:03:24 -0100

    Update Instalar pacotes.cmd

--- Instalar pacotes.cmd
+++ Instalar pacotes.cmd
@@ -1,2 +1,15 @@
 pip install -r requirements.txt
-python main.py
+cls
+@echo off
+echo Diga 1 para escolher Pracas ou 2 para escolher Graduados
+set /p choice=Escolha: 
+
+if "%choice%"=="1" (
+    python Pracas.py
+) else if "%choice%"=="2" (
+    python Graduados.py
+) else (
+    echo Escolha inválida. Por favor, escolha 1 ou 2.
+)
+
+pause
